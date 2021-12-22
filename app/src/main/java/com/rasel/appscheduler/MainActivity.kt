06188b062d71940e92ce200668fe595bef274cc1
rasel.003 <rasel.00x@gmail.com>
2021-12-23 01:22:52 +0600

    retrieved package info from device

--- app/src/main/java/com/rasel/appscheduler/MainActivity.kt
+++ app/src/main/java/com/rasel/appscheduler/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.rasel.appscheduler
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.rasel.appscheduler.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+        setContentView(binding.root)
+
+        val installedApps = getInstalledApps(this)
+
+        var text = ""
+
+        installedApps?.forEachIndexed { index, packageInfo ->
+            text += "\n"+ index + packageInfo.packageName
+        }
+
+        binding.mainText.text = text
+    }
+
+
+    fun getInstalledApps(ctx: Context): Set<PackageInfo>? {
+        val packageManager: PackageManager = ctx.getPackageManager()
+        val allInstalledPackages = packageManager.getInstalledPackages(PackageManager.GET_META_DATA)
+        val filteredPackages: MutableSet<PackageInfo> = HashSet()
+        val defaultActivityIcon = packageManager.defaultActivityIcon
+        for (each in allInstalledPackages) {
+            if (ctx.getPackageName().equals(each.packageName)) {
+                continue  // skip own app
+            }
+            try {
+                // add only apps with application icon
+                val intentOfStartActivity =
+                    packageManager.getLaunchIntentForPackage(each.packageName)
+                        ?: continue
+                val applicationIcon = packageManager.getActivityIcon(intentOfStartActivity)
+                if (applicationIcon != null && defaultActivityIcon != applicationIcon) {
+                    filteredPackages.add(each)
+                }
+            } catch (e: PackageManager.NameNotFoundException) {
+                Log.i("MyTag", "Unknown package name " + each.packageName)
+            }
+        }
+        return filteredPackages
     }
 }
